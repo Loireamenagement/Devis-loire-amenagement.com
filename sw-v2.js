63f33a07c93438480c8f6130ad44dc176368eea7
Loireamenagement <gerant@loire-amenagement.com>
2026-07-01 09:14:25 +0200

    Add files via upload

--- sw-v2.js
+++ sw-v2.js
@@ -1,4 +1,4 @@
-const CACHE = 'la-v2';
+const CACHE = 'la-v3';
 self.addEventListener('install', e => e.waitUntil(
   caches.open(CACHE).then(c => c.addAll(['/app-v2.html','/manifest-v2.json'])).then(() => self.skipWaiting())
 ));
