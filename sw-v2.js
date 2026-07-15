7da2c7cfc064014a497aaff5f33a4e9933ec09da
Loireamenagement <gerant@loire-amenagement.com>
2026-07-15 15:44:20 +0200

    sw-v2.js

--- sw-v2.js
+++ sw-v2.js
@@ -1,4 +1,4 @@
-const CACHE = 'la-v3';
+const CACHE = 'la-v5';
 self.addEventListener('install', e => e.waitUntil(
   caches.open(CACHE).then(c => c.addAll(['/app-v2.html','/manifest-v2.json'])).then(() => self.skipWaiting())
 ));
